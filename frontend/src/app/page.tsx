0f7c363aeb1c169b89d6c59fad4e49fcf23ccdf7
Ekke <tristan.uibo@outlook.com>
2023-12-17 01:45:28 +0200

    initial build

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import Head from 'next/head'
+export default function Home() {
+  return (
+      <div >
+        <Head>
+          <title>IrieSphere</title>
+        </Head>
+        <h1>Rastafari social-network</h1>
+      </div>
+  )
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module social-network
+
+go 1.21.0
+
+require (
+	github.com/gorilla/mux v1.8.1
+	github.com/mattn/go-sqlite3 v1.14.18
+)
