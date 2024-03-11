f191b2edff9034174c03ef131b65c6deec792b97
Anton <anton.urban.est@gmail.com>
2024-03-11 15:27:22 +0200

    Fix chatbox message input as empty
    Add a check for the chatbox message field. If it's empty it won't let the user submit a message
    SN-27

--- frontend/src/components/chat/MessageInput.tsx
+++ frontend/src/components/chat/MessageInput.tsx
@@ -1,5 +1,6 @@
 // components/MessageInput.tsx
 import React, { useState } from 'react';
+import InputEmoji from "react-input-emoji";
 
 interface MessageInputProps {
     onSend: (message: string) => void;
@@ -10,9 +11,13 @@ const MessageInput: React.FC<MessageInputProps> = ({ onSend }) => {
 
     const handleSubmit = (e: React.FormEvent) => {
         e.preventDefault();
-        onSend(text);
-        setText('');
-    };
+    
+        // Check if the message is not empty before sending
+        if (text.trim() !== '') {
+          onSend(text);
+          setText('');
+        }
+      };
 
     return (
         <form onSubmit={handleSubmit} className="input-container p-4 border-t">
