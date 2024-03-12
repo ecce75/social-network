d49644dd0ea9a46156c331cbee3ab5ff525fa876
Anton <anton.urban.est@gmail.com>
2024-03-12 18:16:27 +0200

    Integrate emojis to private chat messages
    Add an emojipicker dependency (emoji-mart) to direct messages. Add a send button and a recreational spliffy button for emojipicker wagwaan
    SN-29

--- frontend/src/components/chat/MessageInput.tsx
+++ frontend/src/components/chat/MessageInput.tsx
@@ -1,36 +1,104 @@
 // components/MessageInput.tsx
-import React, { useState } from 'react';
-import InputEmoji from "react-input-emoji";
+import React, { useState, useRef, useEffect } from 'react';
+import { FaJoint } from 'react-icons/fa';
+import data from '@emoji-mart/data';
+import Picker from '@emoji-mart/react';
 
 interface MessageInputProps {
-    onSend: (message: string) => void;
+  onSend: (message: string) => void;
 }
 
 const MessageInput: React.FC<MessageInputProps> = ({ onSend }) => {
-    const [text, setText] = useState('');
+  const [text, setText] = useState('');
+  const [showEmoji, setShowEmoji] = useState(false);
+  const emojiButtonRef = useRef<HTMLButtonElement>(null);
 
-    const handleSubmit = (e: React.FormEvent) => {
-        e.preventDefault();
-    
-        // Check if the message is not empty before sending
-        if (text.trim() !== '') {
-          onSend(text);
-          setText('');
-        }
-      };
+  // add emoji
+  const addEmoji = (e: { unified: string }) => {
+    const sym = e.unified.split('_');
+    const codeArray = sym.map((el) => parseInt(el, 16));
+    const emoji = String.fromCodePoint(...codeArray);
+    setText((prevText) => prevText + emoji);
+  };
 
-    return (
-        <form onSubmit={handleSubmit} className="input-container p-4 border-t">
-            <input
-                type="text"
-                value={text}
-                onChange={(e) => setText(e.target.value)}
-                className="input bg-gray-100 p-2 rounded-lg w-full border-gray-300"
-                placeholder="Type a message..."
-            />
-            <button type="submit" className="send-button hidden">Send</button>
-        </form>
-    );
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    // Check if the message is not empty before sending
+    if (text.trim() !== '') {
+      onSend(text);
+      setText('');
+    }
+  };
+
+  const handleDocumentClick = (e: MouseEvent) => {
+    // Close the emoji picker if the click is outside the emoji button and picker
+    if (
+      emojiButtonRef.current &&
+      !emojiButtonRef.current.contains(e.target as Node) &&
+      !document.querySelector('.emoji-picker')?.contains(e.target as Node)
+    ) {
+      setShowEmoji(false);
+    }
+  };
+
+  useEffect(() => {
+    document.addEventListener('click', handleDocumentClick);
+
+    return () => {
+      document.removeEventListener('click', handleDocumentClick);
+    };
+  }, []);
+
+  return (
+    <form onSubmit={handleSubmit} className="input-container p-2 border-t">
+      <input
+        type="text"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+        className="input bg-gray-100 p-2 rounded-lg w-full border-gray-300"
+        placeholder="Type a message..."
+      />
+      <div style={{ display: 'flex' }}>
+        <button
+          type="submit"
+          className="send-button"
+          style={{ backgroundColor: 'darkgreen', borderRadius: 10, padding: 0, marginBlockStart: 4, width: 200 }}
+        >
+          <strong>Send</strong>
+        </button>
+        <button
+          type="button"
+          onClick={() => setShowEmoji(!showEmoji)}
+          className='emoji'
+          ref={emojiButtonRef}
+          style={{
+            color: 'darkgreen',
+            backgroundColor: 'lightgreen',
+            borderRadius: 25,
+            width: 48,
+            height: 30,
+            marginInlineStart: 30,
+            marginBlockStart: 4,
+            paddingInlineStart: 10,
+            fontSize: 25,
+          }}
+        >
+          <FaJoint />
+        </button>
+
+        {showEmoji && <div className='absolute bottom-[4%] right-80'>
+          <Picker data={data}
+            emojiSize={20}
+            emojiButtonSize={30}
+            onEmojiSelect={addEmoji}
+            maxFrequentRows={0}
+          />
+        </div>}
+      </div>
+    </form>
+  );
 };
 
 export default MessageInput;
+
